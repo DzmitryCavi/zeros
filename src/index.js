c561f61f2a2ed8ab09599dea9ac8a817331a4aaf
Dzmitry Malakhouski <dzmitry.malakhouski@tut.by>
2019-10-10 10:38:22 +0300

    feat: add function zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let sumOfZeros = 0;
+  let arrayFromString = expression.split("");
+  let num = ""; 
+  let fucNum ="";
+  let arrayOfNum = [];
+  arrayFromString.push("*");
+  
+  arrayFromString.forEach(element => {
+    if(element !="!" && element !="*"){
+      num += element;
+    } else if (element == "!"){
+        fucNum += element;
+    } else {
+      for (var i = BigInt(Number(num)), sum = BigInt(1) ; i>0 ; i -= BigInt(fucNum.length)){
+        sum *= i;
+      }
+      arrayOfNum.push(BigInt(sum));
+      sum = 1;
+      num = "";
+      fucNum = "";
+    }
+  });
+
+  let ProcessingString = BigInt(arrayOfNum.reduce((a,b)=>a*b));
+  ProcessingString = ProcessingString.toString();
+  let j = ProcessingString.length-1 ;
+  
+  while(true){ 
+    if(ProcessingString[j] == '0'){
+      sumOfZeros++ ;
+      j-- ;
+    } else { break; }
+  }
+
+  return sumOfZeros;
 }
+
